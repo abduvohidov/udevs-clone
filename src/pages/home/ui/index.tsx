b5ec8eba62c54b236c9adf5164ba0658fc218571
Abdulloh <abduvohidov.abdullokh@gmail.com>
2024-07-15 10:25:00 +0500

    clean homepage

--- src/pages/home/ui/index.tsx
+++ src/pages/home/ui/index.tsx
@@ -1,13 +1,4 @@
-import { Container } from "@/shared/ui/grid";
-import { InputText } from "@/shared/ui/input-text";
-
 export const HomePage = () => {
-  // return <><h1>Home page</h1></>
-  return (
-    <>
-      <Container className="mt-5">
-        <InputText labelText="Имя"/>
-      </Container>
-    </>
-  );
+
+  return <><h1>Home page</h1></>
 };
