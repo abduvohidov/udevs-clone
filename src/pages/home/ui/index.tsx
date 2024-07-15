ce5a3b7d72eae0af41c2a64652f23d10992d9aa1
Abdulloh <abduvohidov.abdullokh@gmail.com>
2024-07-15 07:54:04 +0500

    clean home page

--- src/pages/home/ui/index.tsx
+++ src/pages/home/ui/index.tsx
@@ -1,21 +1,4 @@
 export const HomePage = () => {
-	
-  const Section = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-    background: "black",
-  };
 
-  return (
-    <div style={Section}>
-      <div>
-        <img
-          src="https://media.licdn.com/dms/image/D4D12AQEwCsGPVYamBw/article-cover_image-shrink_600_2000/0/1698392997369?e=2147483647&v=beta&t=XMO_upn0DfifUHLOA352hdcH71tM5yo9RtER-6JHpyw"
-          alt="img"
-        />
-      </div>
-    </div>
-  );
+  return <><h1>Home page</h1></>
 };
