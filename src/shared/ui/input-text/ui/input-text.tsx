36ca77c437a8b20f473ddcafec5dc04259e7303f
Abdulloh <abduvohidov.abdullokh@gmail.com>
2024-07-19 16:28:14 +0500

    fixed some bugs in input text

--- src/shared/ui/input-text/ui/input-text.tsx
+++ src/shared/ui/input-text/ui/input-text.tsx
@@ -5,39 +5,40 @@ import { useState } from "react";
 interface IInputText {
   labelText: string;
   type?: string;
-  className?: Array<string>;
+  className?: string
 }
 
-export const InputText = ({ type = "text", labelText, className }: IInputText) => {
-    const [value, setValue] = useState('');
-    const [isFocused, setIsFocused] = useState(false);
-  
-    const handleFocus = () => {
+export const InputText = ({ type = "text", labelText, className}: IInputText) => {
+  const [value, setValue] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    if (!value) {
       setIsFocused(true);
-    };
-  
-    const handleBlur = () => {
-      if (!value) {
-        setIsFocused(true);
-      }
-    };
+    }
+  };
 
   return (
     <>
-    {isFocused && !value && (
+      {isFocused && !value && (
         <h4 className={cls.RequiredMessage}>Заполните поле</h4>
       )}
-    <div className={cls.InputWrapper}>
-      <input
-        className={classNames([cls.Input, cls.InputText])}
-        type={type}
-        autoComplete="off"
-        placeholder=""
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <label className={cls.InputLabel}>{labelText}</label>
-    </div></>
+      <div className={classNames([cls.InputWrapper, className])}>
+        <input
+          className={classNames([cls.Input, cls.InputText])}
+          type={type}
+          autoComplete="off"
+          placeholder=""
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <label className={cls.InputLabel}>{labelText}</label>
+      </div>
+    </>
   );
 };
