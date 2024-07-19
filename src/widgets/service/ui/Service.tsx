31a9b6698f1b45150b82b3d73acc68d5f108807c
Muha11ad <abdushukurovmukhammadjon99@gmail.com>
2024-07-19 12:24:04 +0500

    All done in service

---
+++ src/widgets/service/ui/Service.tsx
@@ -0,0 +1,19 @@
+import { ServiceCard } from "@/enitites/service-card/index";
+import { SERVICE_CONTENT } from "@/shared/lib/contents/service-content";
+import { Container } from "@/shared/ui/grid/container";
+import { Title } from "@/shared/ui/title/title";
+import "./Service.scss";
+
+export const Service = () => {
+	const content = SERVICE_CONTENT.map((object) => {
+		return <ServiceCard title={object.title} image={object.image} />;
+	});
+	return (
+		<section id="/service">
+			<Container>
+				<Title>Наши услуги</Title>
+				<div className="service-content-wrapper">{content}</div>
+			</Container>
+		</section>
+	);
+};
