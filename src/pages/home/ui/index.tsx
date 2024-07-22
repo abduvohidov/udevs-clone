b0560bd23f9dd775c4a6a73e98341a6eb2937c98
Abdulloh <abduvohidov.abdullokh@gmail.com>
2024-07-22 19:43:45 +0500

    SectionDesign

--- src/pages/home/ui/index.tsx
+++ src/pages/home/ui/index.tsx
@@ -4,6 +4,7 @@ import { OurTeam } from "@/widgets/our-team/index";
 import { SectionMobile } from "@/pages/home/ui/components/section-mobile";
 import { Service } from "@/widgets/service";
 import { SectionErpSystem } from "./components/section-erp-system";
+import { SectionDesign } from "./components/section-design";
 
 export const HomePage = () => {
   // return <><h1>Home page</h1></>
@@ -14,6 +15,7 @@ export const HomePage = () => {
       <OurTeam />
       <SectionMobile />
       <SectionErpSystem />
+      <SectionDesign />
       <Contacts />
     </>
   );
